88bd8f0c9f08c301a514a201508b7889d9ed2938
DanielVG <danielveragi@gmail.com>
2017-05-13 19:42:49 +0200

    Changes on the Miwok app. Learned about OOP and ArrayAdapters as well as adding a new App about Array Adapters(Flauvours)

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -20,25 +20,20 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
         //TODO: Add Words here
 //        Creating ArrayList of Strings
-        ArrayList<String> words = new ArrayList<String>();
-        words.add(getResources().getString(R.string.eng_1));
-        words.add(getResources().getString(R.string.eng_2));
-        words.add(getResources().getString(R.string.eng_3));
-        words.add(getResources().getString(R.string.eng_4));
-        words.add(getResources().getString(R.string.eng_5));
-        words.add(getResources().getString(R.string.eng_6));
-        words.add(getResources().getString(R.string.eng_7));
-        words.add(getResources().getString(R.string.eng_8));
-        words.add(getResources().getString(R.string.eng_9));
-        words.add(getResources().getString(R.string.eng_10));
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word(getResources().getString(R.string.eng_1), getResources().getString(R.string.miwok_1)));
+        words.add(new Word(getResources().getString(R.string.eng_2), getResources().getString(R.string.miwok_2)));
+        words.add(new Word(getResources().getString(R.string.eng_3), getResources().getString(R.string.miwok_3)));
+        words.add(new Word(getResources().getString(R.string.eng_4), getResources().getString(R.string.miwok_4)));
+        words.add(new Word(getResources().getString(R.string.eng_5), getResources().getString(R.string.miwok_5)));
+        words.add(new Word(getResources().getString(R.string.eng_6), getResources().getString(R.string.miwok_6)));
+        words.add(new Word(getResources().getString(R.string.eng_7), getResources().getString(R.string.miwok_7)));
+        words.add(new Word(getResources().getString(R.string.eng_8), getResources().getString(R.string.miwok_8)));
+        words.add(new Word(getResources().getString(R.string.eng_9), getResources().getString(R.string.miwok_9)));
+        words.add(new Word(getResources().getString(R.string.eng_10), getResources().getString(R.string.miwok_10)));
 
-        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
-        // adapter knows how to create layouts for each item in the list, using the
-        // simple_list_item_1.xml layout resource defined in the Android framework.
-        // This list item layout contains a single {@link TextView}, which the adapter will set to
-        // display a single word.
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+//        The Adapter shows how to create list of Items in every View
+        WordAdapter adapter = new WordAdapter(this, words);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -49,23 +44,7 @@ public class NumbersActivity extends AppCompatActivity {
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(adapter);
 
-
-//        For Loop example
-//        for (int i = 0; i < words.size(); i++){
-//
-//            // Find the root view so we can add child views to it
-//            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-//
-//            // Create a new TextView
-//            TextView wordView = new TextView(this);
-//
-//            // Set the text to be word at the current index
-//            wordView.setText(words.get(i));
-//
-//            // Add this TextView as another child to the root view of this layout
-//            rootView.addView(wordView);
-//        }
     }
 }
---
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DanielVG on 13.05.17.
+ */
+
+public  class WordAdapter extends ArrayAdapter<Word> {
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentAndroidFlavor = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        miwokTextView.setText(curentWord.getVersionName());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        numberTextView.setText(currentAndroidFlavor.getVersionNumber());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
